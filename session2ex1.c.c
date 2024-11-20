2516e88a0dc8571b5bcedf48c834b76e9eb317aa
Quang-1310 <hoangminhquang1310@gmail.com>
2024-11-20 13:45:24 +0700

    Delete session2ex1.c.c

--- session2ex1.c.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a=12; // kieu du lieu so nguyen
-	float b=3.14; // kieu du lieu so thuc
-	char c='A';// kieu du lieu ky tu 
-	bool d=true; // kieu du lieu boolean
-	return 0;
-}
