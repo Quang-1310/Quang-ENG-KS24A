ea629f64f9e6424f686425da693b61792a184eb3
Quang-1310 <hoangminhquang1310@gmail.com>
2024-11-20 13:19:36 +0700

    session 2 ex 5

---
+++ session2_ex5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+	int a = 5;// chieu dai
+	int b = 6;// chieu rong
+	int chuvi = (a+b)*2;
+	int dientich = a * b;
+	printf("chu vi hinh chu nhat la: %d\n", chuvi);
+	printf("dien tich hinh chu nhat la: %d\n", dientich);
+	return 0;
+}
