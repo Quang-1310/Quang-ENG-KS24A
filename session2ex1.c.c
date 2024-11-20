34508000047a40858b5994f99c14af667a378237
Quang-1310 <hoangminhquang1310@gmail.com>
2024-11-20 13:45:51 +0700

    session 2 ex 1.c

---
+++ session2ex1.c.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+	int a=12; // kieu du lieu so nguyen
+	float b=3.14; // kieu du lieu so thuc
+	char c='A';// kieu du lieu ky tu 
+	bool d=true; // kieu du lieu boolean
+	return 0;
+}
