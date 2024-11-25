f67e038edb0c088901880db416f27d15867aec7b
Quang-1310 <hoangminhquang1310@gmail.com>
2024-11-25 21:00:23 +0700

    ss5_ex3

---
+++ ss5_ex3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n, sum = 0;
+	printf(" moi ban nhap vao 1 so nguyen duong ");
+	scanf("%d", &n);
+	for(int i=1; i <= n; i++){
+		sum += i;
+	}
+	printf("tong cac so tu 1 den %d la %d", n, sum);
+	return 0;
+}
