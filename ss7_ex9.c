21a2e28d9bb7bd369ea7365b6d6c12f76c9687df
Quang-1310 <hoangminhquang1310@gmail.com>
2024-11-27 22:50:58 +0700

    ss7_ex9

---
+++ ss7_ex9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	int m, n;
+	printf("Moi ban nhap vao so hang va so cot cua mang ");
+	scanf("%d %d",&m, &n);
+	int a[m][n];
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			printf("Moi ban nhap vao phan tu o hang %d cot %d ",i + 1, j + 1);
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			if(i == 0 || i == m -1 || j == 0 || j == n - 1 ){
+				printf("%d ",a[i][j]); 
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
